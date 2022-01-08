875633c18333aae5bb2c880c8b0d962c5567ba2f
Rea <rmrea@kenya-airways.com>
2022-01-08 12:08:30 +0300

    add directive to display quote on html

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,7 +1,7 @@
 export class Quote {
     public showDescription: boolean;
     
-    constructor(public id: number,public author: string,public quote: string, public upVote: number, public downVote: number, public createDate: Date){
+    constructor(public id: number,public author: string,public quote: string, public upVote: number, public downVote: number, public createdDate: Date){
     this.showDescription=false;
   }
 }
