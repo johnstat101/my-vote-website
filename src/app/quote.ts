24f3231adc040b37ce4a0fc586a24bfc79012254
Rea <rmrea@kenya-airways.com>
2022-01-08 09:31:45 +0300

    add quote form app to the quote display html

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,2 +1,7 @@
 export class Quote {
+    public showDescription: boolean;
+    
+    constructor(public id: number,public author: string,public description: string, public completeDate: Date){
+    this.showDescription=false;
+  }
 }
