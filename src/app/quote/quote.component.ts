56e04a874bc3b063e69183a1d9e2e533122444ed
Rea <rmrea@kenya-airways.com>
2022-01-08 13:25:24 +0300

    add app quote details

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -7,7 +7,9 @@ import { Quote } from '../quote';
   styleUrls: ['./quote.component.css']
 })
 export class QuoteComponent implements OnInit {
-  quotes:Quote[]=[]
+  quotes:Quote[]=[
+    new Quote(1,'john Kimani','Never outshine your master',0,0,new Date()),
+  ]
   
   addNewQuote(quote:any){
     let quoteLength = this.quotes.length;
