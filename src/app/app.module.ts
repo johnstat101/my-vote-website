dd19b2647594deb2f6adca98151544b6b26113ca
Rea <rmrea@kenya-airways.com>
2022-01-10 01:24:20 +0300

    add custom directive highlighter using cli

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { QuoteComponent } from './quote/quote.component';
 import { QuoteDetailsComponent } from './quote-details/quote-details.component';
 import { QuoteFormComponent } from './quote-form/quote-form.component';
 import { DateCountPipe } from './date-count.pipe';
+import { HighlighterDirective } from './highlighter.directive';
 
 @NgModule({
   declarations: [
@@ -15,7 +16,8 @@ import { DateCountPipe } from './date-count.pipe';
     QuoteComponent,
     QuoteDetailsComponent,
     QuoteFormComponent,
-    DateCountPipe
+    DateCountPipe,
+    HighlighterDirective
   ],
   imports: [
     BrowserModule,
